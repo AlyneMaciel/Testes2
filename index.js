dbdbec42a0c7eeff91c9a3582bef136420beeb19
Alyne <alyneramaciel@gmail.com>
2020-11-04 22:50:02 +0000

    Test comit

---
+++ index.js
@@ -0,0 +1,13 @@
+let nomeVelha = prompt("Qual o nome da pessoa mais velha?")
+
+let idadeVelha = prompt("Qual a idade da pessoa mais velha?")
+
+let nomeNova = prompt("Qual o nome da pessoa mais nova?")
+
+let idadeNova = prompt("Qual a idade da pessoa mais nova?")
+
+let diferencaIdade = idadeVelha - idadeNova
+
+alert("Pessoa mais velha: " + nomeVelha + " , " + idadeVelha + " anos." +
+"\n\nPessoa mais nova: " + nomeNova + " , " + idadeNova + " anos." +
+"\n\nDiferenca de idade: " + diferencaIdade + " anos.")
